4e66a0ef3913bf5128ee72b98c006058ff2c1771
AnandhuRs619 <ananthurs619@gmail.com>
2025-01-29 18:04:15 +0530

    basics of unit testing

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,4 +1,12 @@
 export const add = (num1,num2)=>{
+    if( typeof num1 !== "number" || typeof num2 !== "number"){
+        return 0;
+    }
 
     return num1 + num2 ;
 } 
+
+
+export const reverseString = (value) =>{
+return value.split("").reverse().join("");
+}
--- src/utils/helper.test.js
+++ src/utils/helper.test.js
@@ -1,10 +1,26 @@
 import { describe , it , test ,expect } from "vitest";
-import { add } from "./helper";
+import { add,reverseString } from "./helper";
 
 
 describe(" test add function",()=>{
 it("returns 1+1 = 2",()=>{
 expect(add(1,1)).toBe(2)
 });
+it("returns 500+500 = 1000",()=>{
+    expect(add(500,500)).toBe(1000)
+    });
+it("checking string value",()=>{
+    expect(add("1","1")).toBe(0)
+    });
+it("checking Array value",()=>{
+    expect(add(["1"],["1"])).toBe(0)
+    });
 
+})
+
+
+describe('Testing reverseString', () => {
+  it(' checking if string reverses',()=>{
+expect(reverseString("anandhu")).toBe("uhdnana")
+  });
 })
